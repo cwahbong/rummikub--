389b8676fa6488674abaf621e28a144cb3fd4b18
cwahbong <cw.ahbong@gmail.com>
2013-01-09 20:48:44 +0800

    Add core skeleton and class "Tile".

---
+++ core/include/Tile.h
@@ -0,0 +1,30 @@
+#ifndef RUMMIKUB_CORE__TILE_H
+#define RUMMIKUB_CORE__TILE_H
+
+namespace rummikub {
+namespace core {
+
+class Tile {
+private:
+  Tile(const Tile&);
+  Tile& operator=(const Tile&);
+
+public:
+  enum Color {RED, BLUE, YELLOW, BLACK};
+  typedef unsigned char Value;
+
+private:
+  Color m_color;
+  Value m_value;
+
+public:
+  Tile(Color, Value);
+
+  Color getColor() const;
+  Value getValue() const;
+};
+
+} // namespace core
+} // namespace rummikub
+
+#endif // RUMMIKUB_CORE__TILE_H
---
+++ core/src/Tile.cpp
@@ -0,0 +1,23 @@
+#include "Tile.h"
+
+namespace rummikub {
+namespace core {
+
+Tile::Tile(Color color, Value value)
+  : m_color(color), m_value (value)
+{/* Empty. */}
+
+Tile::Color
+Tile::getColor() const
+{
+  return m_color;
+}
+
+Tile::Value
+Tile::getValue() const
+{
+  return m_value;
+}
+
+} // namespace core
+} // namespace rummikub
