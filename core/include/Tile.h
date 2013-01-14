72955b5d2c2a87e587136403d702e140fdadad47
cwahbong <cw.ahbong@gmail.com>
2013-01-14 15:57:32 +0800

    Fully implement Tile and its type trait check.

--- core/include/Tile.h
+++ core/include/Tile.h
@@ -1,28 +1,59 @@
 #ifndef RUMMIKUB_CORE__TILE_H
 #define RUMMIKUB_CORE__TILE_H
 
+#include "CoreFwd.h"
+
+#include <type_traits>
+
 namespace rummikub {
 namespace core {
 
 class Tile {
 private:
-  Tile(const Tile&);
-  Tile& operator=(const Tile&);
+  Tile(const Tile&) = delete;
+  const Tile& operator=(const Tile&) = delete;
 
 public:
   enum Color {RED, BLUE, YELLOW, BLACK};
   typedef unsigned char Value;
+  static const Value NO_VALUE = -1;
+  static const Value MIN_VALUE = 1;
+  static const Value MAX_VALUE = 13;
 
 private:
   Color m_color;
   Value m_value;
+  bool m_joker;
+
+  Tile(Color, Value, bool) noexcept;
 
 public:
-  Tile(Color, Value);
+  Tile(Color, Value) noexcept;
+  static Tile joker(Color) noexcept;
 
-  Color getColor() const;
-  Value getValue() const;
+  Tile(Tile&&) noexcept = default;
+  Tile& operator=(Tile&&) noexcept = default;
+
+  Color getColor() const noexcept;
+  Value getValue() const noexcept;
+  bool isJoker() const noexcept;
+
+  friend bool operator==(const Tile&, const Tile&) noexcept;
+  friend bool operator!=(const Tile&, const Tile&) noexcept;
 };
+
+static_assert(!std::is_default_constructible<Tile>::value,
+              "Tile should NOT be default constructible.");
+static_assert(!std::is_copy_constructible<Tile>::value,
+              "Tile should NOT be copy constructible.");
+static_assert(!std::is_copy_assignable<Tile>::value,
+              "Tile should NOT be copy assignable.");
+static_assert(std::is_nothrow_move_constructible<Tile>::value,
+              "Tile should BE nothrow move constructible.");
+static_assert(std::is_nothrow_move_assignable<Tile>::value,
+              "Tile should BE nothrow move assignable.");
+static_assert(std::is_destructible<Tile>::value,
+              "Tile should BE destructible.");
 
 } // namespace core
 } // namespace rummikub
--- core/src/Tile.cpp
+++ core/src/Tile.cpp
@@ -3,20 +3,51 @@
 namespace rummikub {
 namespace core {
 
-Tile::Tile(Color color, Value value)
-  : m_color(color), m_value (value)
+Tile::Tile(Color color, Value value, bool joker) noexcept
+  : m_color(color),
+    m_value(value),
+    m_joker(joker)
 {/* Empty. */}
 
+Tile::Tile(Color color, Value value) noexcept
+  : Tile(color, value, false)
+{/* Empty. */}
+
+Tile
+Tile::joker(Color color) noexcept
+{
+  return Tile{color, NO_VALUE, true};
+}
+
 Tile::Color
-Tile::getColor() const
+Tile::getColor() const noexcept
 {
   return m_color;
 }
 
 Tile::Value
-Tile::getValue() const
+Tile::getValue() const noexcept
 {
   return m_value;
+}
+
+bool
+Tile::isJoker() const noexcept
+{
+  return m_joker;
+}
+
+bool
+operator==(const Tile& lhs, const Tile& rhs) noexcept
+{
+  return (lhs.m_color == rhs.m_color) && (lhs.m_value == rhs.m_value);
+}
+
+inline
+bool
+operator!=(const Tile& lhs, const Tile& rhs) noexcept
+{
+  return ! (lhs == rhs);
 }
 
 } // namespace core
