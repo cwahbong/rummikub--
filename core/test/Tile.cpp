eb739c9d4c63430352c2d80ed2682522900e3235
cwahbong <cw.ahbong@gmail.com>
2013-02-12 17:24:25 +0800

    Update test for class Tile.

--- core/test/Tile.cpp
+++ core/test/Tile.cpp
@@ -6,13 +6,13 @@ namespace rummikub {
 namespace core {
 
 TEST(TileTest, InitTile) {
-  Tile joker{Tile::joker(Tile::BLACK)};
-  EXPECT_EQ(Tile::BLACK, joker.getColor());
-  EXPECT_EQ(Tile::NO_VALUE, joker.getValue());
+  Tile joker{Tile::joker(BLACK)};
+  EXPECT_EQ(BLACK, joker.getColor());
+  EXPECT_EQ(Tile::JOKER_VALUE, joker.getValue());
   EXPECT_TRUE(joker.isJoker());
 
-  Tile t1{Tile::RED, 5};
-  EXPECT_EQ(Tile::RED, t1.getColor());
+  Tile t1{RED, 5};
+  EXPECT_EQ(RED, t1.getColor());
   EXPECT_EQ(5, t1.getValue());
   EXPECT_FALSE(t1.isJoker());
 }
