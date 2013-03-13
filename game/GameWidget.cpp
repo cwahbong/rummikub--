991522041be34387228e9defe1f7610134f86002
cwahbong <cw.ahbong@gmail.com>
2013-03-14 01:19:29 +0800

    Add Game Widget and let it be in MainWindow.

---
+++ game/GameWidget.cpp
@@ -0,0 +1,14 @@
+#include "GameWidget.h"
+#include "ui_GameWidget.h"
+
+GameWidget::GameWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::GameWidget)
+{
+    ui->setupUi(this);
+}
+
+GameWidget::~GameWidget()
+{
+    delete ui;
+}
