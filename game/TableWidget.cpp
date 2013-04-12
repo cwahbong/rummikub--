091f70783eeb222bd076a154053038dab5f7d315
cwahbong <cw.ahbong@gmail.com>
2013-04-12 23:54:11 +0800

    Implement insertSet for TableWidget.

--- game/TableWidget.cpp
+++ game/TableWidget.cpp
@@ -1,6 +1,8 @@
 #include "TableWidget.h"
 #include "ui_TableWidget.h"
 
+#include "SetWidget.h"
+
 using rummikub::core::Set;
 using rummikub::core::Table;
 
@@ -14,6 +16,10 @@ TableWidget::TableWidget(QWidget *parent)
     ui(new Ui::TableWidget)
 {
   ui->setupUi(this);
+  QObject::connect(this, SIGNAL(setInserted(const s_ptr<Set>&)),
+                   this, SLOT(insertSet(const s_ptr<Set>&)));
+  QObject::connect(this, SIGNAL(setRemoved(const s_ptr<Set>&)),
+                   this, SLOT(removeSet(const s_ptr<Set>&)));
 }
 
 TableWidget::~TableWidget()
@@ -28,13 +34,13 @@ TableWidget::bindTable(const s_ptr<Table>& sp_table)
   m_sp_table = sp_table;
   m_insertSetCallback = make_shared<Table::SetCallback>(
     [this](const s_ptr<Set>& sp_set) {
-      // TODO add set widget
+      emit setInserted(sp_set);
     }
   );
   m_sp_table->addInsertSetCallback(m_insertSetCallback);
   m_removeSetCallback = make_shared<Table::SetCallback>(
     [this](const s_ptr<Set>& sp_set) {
-      // TODO remove set widget
+      emit setRemoved(sp_set);
     }
   );
   m_sp_table->addRemoveSetCallback(m_removeSetCallback);
@@ -45,6 +51,22 @@ TableWidget::unbindTable()
 {
   m_sp_table->delInsertSetCallback(m_insertSetCallback);
   m_sp_table->delRemoveSetCallback(m_removeSetCallback);
+}
+
+void
+TableWidget::insertSet(const s_ptr<Set>& sp_set)
+{
+  auto* p_setWidget = new SetWidget();
+  p_setWidget->bindSet(sp_set);
+  layout()->addWidget(p_setWidget);
+}
+
+void
+TableWidget::removeSet(const s_ptr<Set>& sp_set)
+{
+  // TODO
+  /* for (auto* p_SetWidget : findChildren<SetWidget*>()) {
+  } */
 }
 
 } // namespace game
--- game/TableWidget.h
+++ game/TableWidget.h
@@ -17,12 +17,21 @@ namespace game {
 class TableWidget : public QWidget
 {
   Q_OBJECT
+  using Set = core::Set;
     
 public:
   explicit TableWidget(QWidget *parent = 0);
   ~TableWidget();
   void bindTable(const s_ptr<core::Table>&);
   void unbindTable();
+
+signals:
+  void setInserted(const s_ptr<Set>&);
+  void setRemoved(const s_ptr<Set>&);
+
+private slots:
+  void insertSet(const s_ptr<Set>&);
+  void removeSet(const s_ptr<Set>&);
     
 private:
   Ui::TableWidget *ui;
