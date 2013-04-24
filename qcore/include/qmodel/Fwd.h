635785bee6b12dcb2402d8f611f2d9ea40a23d8f
cwahbong <cw.ahbong@gmail.com>
2013-04-24 16:31:08 +0800

    qmodel: Add forward declarations.

---
+++ qcore/include/qmodel/Fwd.h
@@ -0,0 +1,16 @@
+#ifndef RUMMIKUB_QCORE__FWD_H
+#define RUMMIKUB_QCORE__FWD_H
+
+namespace rummikub {
+namespace qmodel {
+
+class Player;
+class Rummikub;
+class Set;
+class Table;
+
+} // namespace qmodel
+} // namespace rummikub
+
+#endif // RUMMIKUB_QCORE__FWD_H
+
