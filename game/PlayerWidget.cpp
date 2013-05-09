513635974d18f6318b41d33af05a3d2244992f71
cwahbong <cw.ahbong@gmail.com>
2013-05-09 22:08:47 +0800

    PlayerWidget binds to qmodel now.

--- game/PlayerWidget.cpp
+++ game/PlayerWidget.cpp
@@ -6,7 +6,8 @@ namespace game {
 
 PlayerWidget::PlayerWidget(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::PlayerWidget)
+    ui(new Ui::PlayerWidget),
+    m_p_player{nullptr}
 {
     ui->setupUi(this);
 }
@@ -14,6 +15,49 @@ PlayerWidget::PlayerWidget(QWidget *parent) :
 PlayerWidget::~PlayerWidget()
 {
     delete ui;
+}
+
+void
+PlayerWidget::bindPlayer(const Player* p_player)
+{
+  m_p_player = p_player;
+  QObject::connect(
+    m_p_player, SIGNAL(tileInserted(const Tile&)),
+    this, SLOT(insertTile(const Tile&))
+  );
+  QObject::connect(
+    m_p_player, SIGNAL(tileRemoved(const Tile&)),
+    this, SLOT(removeTile(const Tile&))
+  );
+  // TODO add tile widgets
+}
+
+void
+PlayerWidget::unbindPlayer()
+{
+  if (m_p_player) {
+    QObject::disconnect(
+      m_p_player, SIGNAL(tileInserted(const Tile&)),
+      this, SLOT(insertTile(const Tile&))
+    );
+    QObject::disconnect(
+      m_p_player, SIGNAL(tileRemoved(const Tile&)),
+      this, SLOT(removeTile(const Tile&))
+    );
+    // TODO remove all tile widgets
+  }
+}
+
+void
+PlayerWidget::insertTile(const Tile&)
+{
+  // TODO
+}
+
+void
+PlayerWidget::removeTile(const Tile&)
+{
+  // TODO
 }
 
 } // namespace game
--- game/PlayerWidget.h
+++ game/PlayerWidget.h
@@ -1,6 +1,11 @@
 #ifndef RUMMIKUB_GAME__PLAYERWIDGET_H
 #define RUMMIKUB_GAME__PLAYERWIDGET_H
 
+#include "StdMemory.h"
+
+#include "qmodel/Player.h"
+#include "qmodel/Tile.h"
+
 #include <QWidget>
 
 namespace Ui {
@@ -12,14 +17,24 @@ namespace game {
 
 class PlayerWidget : public QWidget
 {
-    Q_OBJECT
-    
+  Q_OBJECT
+  using Player = qmodel::Player;
+  using Tile = qmodel::Tile;
+
 public:
-    explicit PlayerWidget(QWidget *parent = 0);
-    ~PlayerWidget();
-    
+  explicit PlayerWidget(QWidget *parent = 0);
+  ~PlayerWidget();
+
+  void bindPlayer(const Player*);
+  void unbindPlayer();
+
+private slots:
+  void insertTile(const Tile&);
+  void removeTile(const Tile&);
+
 private:
-    Ui::PlayerWidget *ui;
+  Ui::PlayerWidget *ui;
+  const Player* m_p_player;
 };
 
 } // namespace game
