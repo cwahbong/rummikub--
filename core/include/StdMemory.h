3c06619a2a43b232c1c942f68495febcc98e259d
cwahbong <cw.ahbong@gmail.com>
2013-08-08 00:00:22 +0800

    Add make_s in StdMemory.h.

--- core/include/StdMemory.h
+++ core/include/StdMemory.h
@@ -2,6 +2,7 @@
 #define RUMMIKUB_CORE__STDMEMORY_H
 
 #include <memory>
+#include <utility>
 
 namespace rummikub {
 
@@ -11,6 +12,12 @@ template <typename T> using  u_ptr = std::unique_ptr<T>;
 template <typename T> using cu_ptr = std::unique_ptr<const T>;
 template <typename T> using  s_ptr = std::shared_ptr<T>;
 template <typename T> using cs_ptr = std::shared_ptr<const T>;
+
+template <typename T, typename... Args>
+s_ptr<T> make_s(Args&&... args)
+{
+  return std::make_shared<T>(std::forward<Args>(args)...);
+}
 
 } // namespace rummikub
 
