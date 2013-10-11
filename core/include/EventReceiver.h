81daedb5347e5966c34bd0d194d4df758337c790
cwahbong <cw.ahbong@gmail.com>
2013-10-11 18:41:41 +0800

    Add documentation for EventReceiver.h

--- core/include/EventReceiver.h
+++ core/include/EventReceiver.h
@@ -6,36 +6,60 @@
 
 namespace rummikub {
 
+/*! \addtogroup rummicore
+ *  @{
+ */
+
+/*! \brief A collection of functions called when something happened in
+ *         game.
+ *
+ *  All of the member functions are virtual with empty implementation.
+ */
 struct EventReceiver {
-  // Invoked by Agent::Delegate.
+  virtual ~EventReceiver(
+  ) {/* Empty. */}
+
+  /*! Invoked when a tile is put into a set on the table. */
   virtual void tilePut(
       const cs_ptr<Player>&,
       const Tile&,
       const cs_ptr<Set>&
   ) {/* Empty. */}
+
+  /*! Invoked when a tile is moved from a set to another set. */
   virtual void tileMoved(
       const cs_ptr<Player>&,
       const Tile&,
       const cs_ptr<Set>&,
       const cs_ptr<Set>&
   ) {/* Empty. */}
+
+  /*! Invoked when actions of a round by an agent restored. */
   virtual void restored(
       const cs_ptr<Player>&,
       const cs_ptr<Table>&
   ) {/* Empty. */}
 
-  // Invoke by rummikub
+  /*! Invoked when the game is going to start. */
   virtual void gameStarted(
   ) {/* Empty. */}
+
+  /*! Invoked when the game is going to end. */
   virtual void gameEnded(
   ) {/* Empty. */}
+
+  /*! Invoked when a turn is going to start. */
   virtual void turnStarted(
     const cs_ptr<Player>&
   ) {/* Empty. */}
+
+  /*! Invoked when a turn is going to end. */
   virtual void turnEnded(
     const cs_ptr<Player>&
   ) {/* Empty. */}
 };
+
+/*! @} */
 
 } // namespace rummikub
 
