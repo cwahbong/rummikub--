01585f9834633db36ce923444980a048ce4cfb80
cwahbong <cw.ahbong@gmail.com>
2013-10-11 10:47:28 +0800

    Add documentation for CoreFwd.h

--- core/include/CoreFwd.h
+++ core/include/CoreFwd.h
@@ -1,6 +1,10 @@
 #ifndef RUMMIKUB_CORE__CORE_FWD_H
 #define RUMMIKUB_CORE__CORE_FWD_H
 
+/*! \file
+ *  \brief Forward declarations of the classes in rummicore.
+ */
+
 namespace rummikub {
 
 class Agent;
