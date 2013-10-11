e55533d1b51e5c5f9180fe4365fbd8cf43f8297e
cwahbong <cw.ahbong@gmail.com>
2013-10-11 10:05:17 +0800

    Add documentation for StdMemory.h

--- core/include/StdMemory.h
+++ core/include/StdMemory.h
@@ -4,20 +4,42 @@
 #include <memory>
 #include <utility>
 
+/*! \file
+ *  \brief Smart pointer types and functions.
+ *
+ *  TODO: let user to use their own smart pointer type.
+ */
+
 namespace rummikub {
 
+/*! \addtogroup rummicore
+ *  @{
+ */
+
+/*! \brief Weak pointer type. */
 template <typename T> using  w_ptr = std::weak_ptr<T>;
+/*! \brief Weak pointer type pointing to const. */
 template <typename T> using cw_ptr = std::weak_ptr<const T>;
+/*! \brief Unique pointer type. */
 template <typename T> using  u_ptr = std::unique_ptr<T>;
+/*! \brief Unique pointer type pointing to const. */
 template <typename T> using cu_ptr = std::unique_ptr<const T>;
+/*! \brief Shared pointer type. */
 template <typename T> using  s_ptr = std::shared_ptr<T>;
+/*! \brief Shared pointer type pointing to const */
 template <typename T> using cs_ptr = std::shared_ptr<const T>;
 
+/*! \brief Shortcut function for making a shared pointer.
+ *
+ *  Currently use std::make_shared.
+ */
 template <typename T, typename... Args>
 s_ptr<T> make_s(Args&&... args)
 {
   return std::make_shared<T>(std::forward<Args>(args)...);
 }
+
+/*! @} */
 
 } // namespace rummikub
 
