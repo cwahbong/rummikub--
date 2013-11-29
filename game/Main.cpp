f8eaabfb47ddd879ad40c93150e2b20cddb02a73
cwahbong <cw.ahbong@gmail.com>
2013-11-30 00:43:36 +0800

    Clean up game/Main.cpp

--- game/Main.cpp
+++ game/Main.cpp
@@ -1,50 +1,19 @@
-// #include "MainConsole.h"
-#include "MainWindow.h"
+#include "StdMemory.h"
+  using rummikub::make_s;
 
-#include <cstring>
-#include <memory>
+#include "MainWindow.h"
+  using rummikub::game::MainWindow;
 
 #include <QApplication>
-#include <QObject>
-#include <QTimer>
-
-// using rummikub::game::MainConsole;
-using rummikub::game::MainWindow;
 
 using std::make_shared;
-using std::shared_ptr;
-using std::strcmp;
-
-inline
-bool
-isUsingGui(int argc, char* argv[]) {
-  bool useGui = true;
-#ifdef Q_WS_X11
-  useGui = getenv("DISPLAY") != 0;
-#endif // Q_WS_X11
-  for (int i=0; i<argc; ++i) {
-    if (strcmp(argv[i], "--nogui")==0) {
-      useGui = false;
-    }
-  }
-  return useGui;
-}
 
 int
 main(int argc, char* argv[])
 {
-  bool useGui = isUsingGui(argc, argv);
-  QApplication app(argc, argv, useGui);
-  shared_ptr<MainWindow> sp_window;
-  // shared_ptr<MainConsole> sp_console;
-  if (useGui) {
-    sp_window = make_shared<MainWindow>();
-    sp_window->show();
-  } else {
-    // sp_console = make_shared<MainConsole>();
-    // QObject::connect(sp_console.get(), SIGNAL(end()), &app, SLOT(quit()));
-    // QTimer::singleShot(0, sp_console.get(), SLOT(start()));
-  }
+  QApplication app(argc, argv);
+  auto sp_window = make_s<MainWindow>();
+  sp_window->show();
   return app.exec();
 }
 
